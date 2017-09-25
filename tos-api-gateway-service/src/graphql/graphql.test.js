65d933f7b0973ccc64b7bd4fdeababc4d6f4d877
vunguyenhung <vunguyenhung@outlook.com>
2017-09-25 22:24:27 +0700

    improve test name

--- tos-api-gateway-service/src/graphql/graphql.test.js
+++ tos-api-gateway-service/src/graphql/graphql.test.js
@@ -3,7 +3,7 @@ const graphql = require('graphql');
 
 describe('GraphQL', () => {
   // how to test the command has converted OK ?
-  it('return success response when send valid RegisterEvent', async () => {
+  it('should return success response when send valid RegisterEvent', async () => {
     // GIVEN
     // language=GraphQL
     const query = `
@@ -32,7 +32,7 @@ describe('GraphQL', () => {
     expect(result).toEqual(expectedResult);
   });
 
-  it('return error response when send invalid RegisterEvent', async () => {
+  it('should return InvalidPayload error response when send invalid RegisterEvent', async () => {
     // GIVEN
     // language=GraphQL
     const query = `
