d26fb7e53fae65f1d6c777431e69bccff079459d
vunguyenhung <vunguyenhung@outlook.com>
2017-10-10 23:27:40 +0700

    create empty test file for send-command.resolver

---
+++ api-gateway-service/src/graphql/resolvers/__tests__/send-command.resolver.test.js
@@ -0,0 +1,9 @@
+// TODO: mock producer.
+xdescribe('GraphQL', () => {
+  // how to test the command has converted OK ?
+  it('should return success response when send valid RegisterEvent', async () => {
+  });
+
+  it('should return InvalidPayload error response when send invalid RegisterEvent', async () => {
+  });
+});
