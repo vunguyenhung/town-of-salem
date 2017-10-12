c22d0e9c981fc84284e9d285115c1fe93168a3f6
vunguyenhung <vunguyenhung@outlook.com>
2017-10-12 19:43:37 +0700

    create basic test

---
+++ authentication-service/src/__tests__/app.test.js
@@ -0,0 +1,8 @@
+const supertest = require('supertest');
+const { app } = require('../app');
+
+describe('App general test', () => {
+  it('should return 200-OK code when request /', () => {
+    supertest(app).get('/').expect(200);
+  });
+});
