a4b72897feb87f9ec6c280a54a4b66cefe01b527
vunguyenhung <vunguyenhung@outlook.com>
2017-10-12 16:39:31 +0700

    remove redundant test

--- api-gateway-service/src/graphql/resolvers/__tests__/send-command.resolver.test.js
+++
@@ -1,9 +0,0 @@
-// TODO: mock producer.
-xdescribe('GraphQL', () => {
-  // how to test the command has converted OK ?
-  it('should return success response when send valid RegisterEvent', async () => {
-  });
-
-  it('should return InvalidPayload error response when send invalid RegisterEvent', async () => {
-  });
-});
