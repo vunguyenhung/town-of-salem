f6cf53d6b9806def456cbd1856302792efda349b
vunguyenhung <vunguyenhung@outlook.com>
2017-10-31 11:47:49 +0700

    handle error when dispatch start user add action. Refactor and write more tests

--- lobby-service/src/usecases/effects/__tests__/index.test.js
+++ lobby-service/src/usecases/effects/__tests__/index.test.js
@@ -4,14 +4,16 @@
 const configureMockStore = require('redux-mock-store').default;
 const thunk = require('redux-thunk').default;
 const uuid = require('uuid/v4');
+const R = require('ramda');
 // const { createLogger } = require('redux-logger');
 
 /*
 Project file imports
  */
-const { StartUserAdd } = require('../');
+const { StartUserAdd, StartLobbyAdd } = require('../');
 const { ActionTypes } = require('../../actions');
-const { addUser } = require('../../../entities');
+const { addUser, addLobby } = require('../../../entities');
+const { createLobby } = require('../../../entities/add-lobby');
 
 const mockStore = configureMockStore([
   thunk,
@@ -19,25 +21,12 @@ const mockStore = configureMockStore([
 ]);
 
 describe('Lobby effect test', () => {
-  // TODO: remove duplication of this
-  const initialLobby = {
-    id: uuid(),
-    users: [],
-    isClosed: false,
-  };
-
-  const initialLobbies = [initialLobby];
-
-  const initialState = {
-    lobbies: initialLobbies,
-  };
-
-  it('should dispatch SetLobbies Action after successfully add user to lobbies', () => {
-    // STUB
-    const store = mockStore(initialState);
-
+  it('should dispatch SetLobbies Action with new user added to an available lobby ' +
+    'when dispatch StartUserAdd when there is an available lobby in lobbies', () => {
     // GIVEN
     const toBeAddedUser = 'vunguyenhung';
+
+    const initialLobbies = [createLobby()];
 
     const expectedPayload = addUser(toBeAddedUser, initialLobbies).merge();
 
@@ -45,23 +34,82 @@ describe('Lobby effect test', () => {
       { type: ActionTypes.SET_LOBBIES, payload: expectedPayload },
     ];
 
+    // STUB
+    const store = mockStore({ lobbies: initialLobbies });
+
     // WHEN
-    store.dispatch(StartUserAdd('vunguyenhung'));
+    store.dispatch(StartUserAdd(toBeAddedUser));
     const actualActions = store.getActions();
 
     // THEN
     expect(actualActions).toEqual(expectedActions);
   });
 
-  it('should handle the abnormal case', () => {
+  it('should dispatch SetLobbies Action with new lobby added lobbies ' +
+    'when dispatch StartLobbyAdd', () => {
+    // GIVEN
+    const initialLobbies = [createLobby()];
+
+    // no need to call merge() here since addLobby return Array Lobby directly
+    const expectedPayload = addLobby(initialLobbies);
+
+    const expectedActions = [
+      { type: ActionTypes.SET_LOBBIES, payload: expectedPayload },
+    ];
+
+    // STUB
+    const store = mockStore({ lobbies: initialLobbies });
+
+    // WHEN
+    // this also return action object
+    console.log('start Lobby Add dispatch: ', store.dispatch(StartLobbyAdd()));
+    const actualActions = store.getActions();
+
+    // THEN
+    expect(actualActions).toEqual(expectedActions);
   });
 
-  it('should handle the error case', () => {
-  });
+  it('should dispatch SetLobbies twice, ' +
+    'first one for adding new lobby and second one for adding new user ' +
+    'when dispatch StartUserAdd thunk when there is no available lobby', () => {
+    // GIVEN
+    // TODO: remove duplication of this.
+    const fullLobby = {
+      id: uuid(),
+      users: R.repeat('vunguyenhung', 15),
+      isClosed: true,
+    };
 
-  it('should handle the edge case', () => {
-  });
+    const fullLobbiesState = {
+      lobbies: [fullLobby],
+    };
 
-  it('should handle some special parameter', () => {
+    const toBeAddedUsername = 'toBeAddedUsername';
+
+    const expectedPayload = [
+      fullLobby,
+      {
+        id: uuid(),
+        users: [toBeAddedUsername],
+        isClosed: false,
+      },
+    ];
+
+    const expectedActions = [
+      { type: ActionTypes.SET_LOBBIES, payload: expectedPayload },
+    ];
+
+    // STUB
+    const store = mockStore(fullLobbiesState);
+
+    // WHEN
+    store.dispatch(StartUserAdd(toBeAddedUsername));
+    const actualActions = store.getActions();
+
+    // THEN
+    expect(actualActions[0].type).toEqual(expectedActions[0].type);
+    expect(actualActions[0].payload[0]).toEqual(expectedActions[0].payload[0]);
+    expect(actualActions[0].payload[1].users).toEqual(expectedActions[0].payload[1].users);
+    expect(actualActions[0].payload[1].isClosed).toEqual(expectedActions[0].payload[1].isClosed);
   });
 });
--- lobby-service/src/usecases/effects/index.js
+++ lobby-service/src/usecases/effects/index.js
@@ -1,15 +1,38 @@
-const { addUser } = require('../../entities');
+/*
+3rd Party library imports
+ */
+const R = require('ramda');
+const Result = require('folktale/result');
+
+/*
+Project file imports
+ */
+const { addUser, addLobby } = require('../../entities');
 const { SetLobbies } = require('../actions');
 const { lobbiesSelector } = require('../reducers');
 
-// (dispatch) => dispatch()
+const StartLobbyAdd = () => (dispatch, getState) =>
+  R.pipe(
+    addLobby,
+    SetLobbies,
+    dispatch,
+  )(lobbiesSelector(getState()));
+
 const StartUserAdd = username => (dispatch, getState) =>
   addUser(username, lobbiesSelector(getState())).matchWith({
     Ok: ({ value }) => dispatch(SetLobbies(value)),
-    // handle error this later
-    // Error: ({ value }) => , // we can nest Result here using dispatch().chain...
+    // TODO: move this to handleLobbyError function when needed
+    Error: ({ value }) => value.matchWith({
+      NoLobbyAvailable: () =>
+        Result.of(addLobby(lobbiesSelector(getState())))
+          .chain(addUser(username))
+          .map(SetLobbies)
+          .map(dispatch)
+          .merge(),
+    }),
   });
 
 module.exports = {
   StartUserAdd,
+  StartLobbyAdd,
 };
