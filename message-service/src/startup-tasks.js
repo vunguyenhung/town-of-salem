e5ca7cb8692b6d9d395e5c64136c359e850a3c80
vunguyenhung <vunguyenhung@outlook.com>
2017-12-30 23:00:35 +0700

    add connect to DB task

--- message-service/src/startup-tasks.js
+++ message-service/src/startup-tasks.js
@@ -9,6 +9,7 @@ const Task = require('folktale/concurrency/task');
 Project file imports
  */
 const { createTrace } = require('./utils');
+const { connect } = require('./database');
 
 const trace = createTrace('src:StartUpService');
 
@@ -30,6 +31,8 @@ const constructTasks = () =>
 		.map(trace('Create topic result: '))
 		.chain(() => createConsumer(config.get('Kafka'), [{ topic: 'tos-message-events' }]))
 		.map(trace('Create consumer result: '))
+		.chain(() => connect(config.get('MongoDB.url')))
+		.map(trace('Connect to DB result: '))
 		.chain(() => onMessage(0));
 
 exports.start = constructTasks;
